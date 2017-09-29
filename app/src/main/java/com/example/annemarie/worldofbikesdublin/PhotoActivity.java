d544ca376fe82eef45f2e9959e95c0e484d0e2cc
Anne-Marie McNamara <annemarie.namara@gmail.com>
2017-09-29 12:23:00 +0100

    take a photo added

--- app/src/main/java/com/example/annemarie/worldofbikesdublin/PhotoActivity.java
+++ app/src/main/java/com/example/annemarie/worldofbikesdublin/PhotoActivity.java
@@ -1,13 +1,45 @@
 package com.example.annemarie.worldofbikesdublin;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class PhotoActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        Button btnCamera = (Button) findViewById(R.id.btnCamera);
+        // this id is in the xml layout file activity_photo
+        ImageView imageView = (Image) findViewById(R.id.imagePhotoView);
+        // this id is in the xml layout file activity_photo
+
+        btnCamera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get ("data");
+        imageView.setImageBitmap(bitmap);
     }
 }
